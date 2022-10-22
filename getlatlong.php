62b2b75e27f0c4c441f7f024a0cbf302c9cef1c0
ragagithub123 <raghass@gmail.com>
2022-10-22 19:32:33 +0530

    removed

--- getlatlong.php
+++ getlatlong.php
@@ -4,7 +4,7 @@ $getsearchtext=$db->joinquery("SELECT `agentid`,`unitnum`,`street`,`suburb`,`cit
 while($row_text=mysqli_fetch_array($getsearchtext)){
 	$newloc=$row_text['unitnum'].",".$row_text['street'].",".$row_text['suburb'].",".$row_text['city'];
 	$location=str_replace(' ', '%20', $newloc);
-$res=file_get_contents("https://maps.googleapis.com/maps/api/geocode/json?address='".$location."'&key=AIzaSyD91uB2dG7Lyj4xOWJwxT-lq9uQ0FtwU0o");
+$res=file_get_contents("https://maps.googleapis.com/maps/api/geocode/json?address='".$location."'&key=yyyyyyy");
 $result_arr=json_decode($res);
 $lat=$result_arr->results[0]->geometry->location->lat;
 $lng=$result_arr->results[0]->geometry->location->lng;
